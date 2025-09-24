80265b71bd6c98770bd171bf9bff207493c9010b
lexendx <tiwaribaibhav2004@gmail.com>
2025-09-25 00:06:13 +0530

    add

--- Condition.java
+++ Condition.java
@@ -5,6 +5,19 @@ public class Condition {
             salary=salary+2000;
         }else {
             salary=salary+1000;
+
+//Multiple if else statement
+if(salary > 10000){
+    salary += 2000;  //salary=salary+2000
+
+}else if(salary > 20000){
+    salary += 3000;
+} else{
+    salary+=1000;
+}
+
+
         }System.out.println(salary);
     }
 }
+
