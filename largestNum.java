47f418eb5bca073f0eb9758bb66ee86a7dca6432
lexendx <tiwaribaibhav2004@gmail.com>
2025-09-25 23:42:37 +0530

    Largest number

---
+++ largestNum.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class largestNum {
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        System.out.println("Enter the first number a");
+        int a = input.nextInt();
+         System.out.println("Enter the first number b");
+        int b = input.nextInt();
+         System.out.println("Enter the first number c");
+        int c = input.nextInt();
+
+
+        // find the largest of the three number.
+ int max = a;
+ if(b> max){
+    max=b;
+ }if(c> max){
+    max = c;
+ }
+ System.out.println(max + " Is the maximum of all these three numbers.");
+    }
+}
