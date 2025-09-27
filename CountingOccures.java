d99a4948eefc9b075a775013ba846a80ff31684b
lexendx <tiwaribaibhav2004@gmail.com>
2025-09-27 23:59:08 +0530

    Array

--- CountingOccures.java
+++ CountingOccures.java
@@ -3,8 +3,10 @@ public class CountingOccures {
      int n[] = {1,3,8,5,7,5,7,8,7,9};   
      int count=0;
      for(int i=0;i<n.length; i++){
+if(n[i]==7){
+    count++;
+}
 
-        
-     }
+     } System.out.println("7 Appears " + count + " times ");
     }
 }
