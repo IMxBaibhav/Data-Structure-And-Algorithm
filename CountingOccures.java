32406dfdf9aeb5fabba33eb63255f95368251c8b
lexendx <tiwaribaibhav2004@gmail.com>
2025-09-27 23:56:43 +0530

    Array

---
+++ CountingOccures.java
@@ -0,0 +1,10 @@
+public class CountingOccures {
+    public static void main(String[] args) {
+     int n[] = {1,3,8,5,7,5,7,8,7,9};   
+     int count=0;
+     for(int i=0;i<n.length; i++){
+
+        
+     }
+    }
+}
