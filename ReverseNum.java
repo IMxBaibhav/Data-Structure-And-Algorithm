e3a35afd48d1e3478c7b771435e73437145cec27
lexendx <tiwaribaibhav2004@gmail.com>
2025-09-29 23:50:58 +0530

    fhof

--- ReverseNum.java
+++ ReverseNum.java
@@ -4,7 +4,7 @@ public class ReverseNum {
         int ans= 0;
         while(num > 0)
         {
-            int rem = num % 10;
+            int rem = num % 10
 
       num /= 10;
      ans = ans * 10 + rem;
