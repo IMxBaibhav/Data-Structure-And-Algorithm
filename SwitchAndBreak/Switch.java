f5aa070e2f4fd9e5ecc67e723d4e2bd3b1587bc7
lexendx <tiwaribaibhav2004@gmail.com>
2025-10-02 23:42:41 +0530

    Switch case

---
+++ SwitchAndBreak/Switch.java
@@ -0,0 +1,21 @@
+package SwitchAndBreak;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+String fruit = in.next();
+
+switch (fruit) {
+    case "Mango":
+    System.out.println("A kings of fruits..");
+break;
+
+case "Apple":
+System.out.println("A red Sweet fruit...");
+break;
+}
+        
+    }
+}
